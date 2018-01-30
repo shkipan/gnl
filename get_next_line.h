3834bba92d3d0dcbce3cf7267f0492ace9a1b696
shkipan <33808396+shkipan@users.noreply.github.com>
2018-01-30 19:58:01 +0200

    buff_size edited

--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10000000000
+# define BUFF_SIZE 1
 # include "./libft/libft.h"
 #include <stdio.h>
 int		get_next_line(const int fd, char **line);
